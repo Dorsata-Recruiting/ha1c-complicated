3890ae2c952bd5325e723a11b73065146b24f1b0
Lin Qiu <theonlylinqiu@gmail.com>
2021-04-06 16:34:09 -0400

    add ruby version and cors

--- ruby-backend/app.rb
+++ ruby-backend/app.rb
@@ -6,6 +6,12 @@ require './ehr_service'
 
 set :port, 3000
 
+before do
+	response.headers["Allow"] = "GET, PUT, POST, DELETE, OPTIONS"
+	response.headers["Access-Control-Allow-Headers"] = "Authorization, Content-Type, Accept, X-User-Email, X-Auth-Token"
+	response.headers["Access-Control-Allow-Origin"] = "*"
+end
+
 get '/' do
 	'Hello world'
 end
